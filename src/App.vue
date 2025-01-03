a5102ee79a8184e87d62ad8f1f100497fbaa508e
vivek <vivek.rawat@anrxsolutions.com>
2025-01-04 01:44:14 +0530

    feat/added-toast

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue';
+import Toaster from './components/ui/toast/Toaster.vue';
 </script>
 
 <template>
   <div class="p-2 md:p-6 max-w-[1500px] mx-auto text-gray-600">
     <Navbar/>
     <RouterView />
+    <Toaster/>
   </div>
 </template>
 
--- src/views/Services/index.vue
+++ src/views/Services/index.vue
@@ -3,16 +3,27 @@ import InfoAlert from './Components/InfoAlert.vue'
 import Service from './Components/Service.vue'
 import Button from '@/components/ui/button/Button.vue'
 import { Plus, MoveRight } from 'lucide-vue-next'
-import { useAlertContent } from '@/stores/shared';
-import { storeToRefs } from 'pinia';
-import {onMounted, ref} from 'vue'
+import { useAlertContent } from '@/stores/shared'
+import { storeToRefs } from 'pinia'
+import { onMounted, ref } from 'vue'
+import { useToast } from '@/components/ui/toast/use-toast'
+import { h } from 'vue'
+import { ToastAction } from '@/components/ui/toast'
+
 const { alertContent } = storeToRefs(useAlertContent())
 const services = ref([1])
+const { toast } = useToast()
 const addingNewServices = () => {
   services.value.push(services.value.length + 1)
 }
+const servicesCreated = () => {
+  toast({
+    title: 'Success',
+    description: 'Service has been created successfully',
+  })
+}
 onMounted(() => {
-        // console.log(alertContent.value)
+  // console.log(alertContent.value)
 })
 // export default {
 //   name: "ServiceSetup",
@@ -22,16 +33,19 @@ onMounted(() => {
   <div class="mx-auto bg-white rounded-lg space-y-6 py-2">
     <h2 class="font-bold text-2xl my-3">Service setup</h2>
     <!-- Header -->
-    <InfoAlert v-if="alertContent"/>
+    <InfoAlert v-if="alertContent" />
     <!-- Service Setup -->
-     <div v-for="service in services" :key="service">
-        <Service />
-     </div>
+    <div v-for="service in services" :key="service">
+      <Service />
+    </div>
 
     <!-- Create Service Button -->
-    <Button variant="outline" class="border-2" @click="addingNewServices()"><Plus class="mb-1" /> Add another service</Button>
-    <Button class="w-full"> Create service <MoveRight class="inline h-5" /> </Button>
+    <Button variant="outline" class="border-2" @click="addingNewServices()"
+      ><Plus class="mb-1" /> Add another service</Button
+    >
+    <Button @click="servicesCreated()" class="w-full">
+      Create service <MoveRight class="inline h-5" />
+    </Button>
   </div>
 </template>
-<style scoped>
-</style>
+<style scoped></style>
